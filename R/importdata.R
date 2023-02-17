f94e47a569e6bbe820651ec3a3d812ecb56a3eaf
t-hashy <t.hashy13@gmail.com>
2023-02-17 11:56:37 +0900

    add google authentication

---
+++ R/importdata.R
@@ -0,0 +1,13 @@
+# Loading -----------
+conf <- config::get()
+load.pkg("googlesheets4") # Google sheets
+load.pkg("googledrive") # Google drive
+load.pkg("tidyverse") # Data manipulation
+
+# Google sheets authentication ---------
+options(gargle_oauth_cache= ".secrets")
+drive_auth(cache = ".secrets", email = conf$G_EMAIL)
+gs4_auth(token = drive_token())
+
+# Import the data ---------
+df <- read_sheet(conf$G_SSID_PL, conf$G_SHTNAME_PL)
